d42e97213fbf46074bfb6e8055a0e0675634b8df
paxom <kladowkd@yandex.ru>
2024-09-19 20:02:08 +0300

    Код к заданию 2.

--- laba2/Source.cpp
+++ laba2/Source.cpp
@@ -3,8 +3,56 @@
 
 using namespace std;
 
-const int size_m = 4000;
-int a[size_m][size_m], b[size_m][size_m], c[size_m][size_m], elem_c;
+const int size_m = 100000;
+int a[size_m];
+
+void shell(int* items, int count)
+{
+
+	int i, j, gap, k;
+	int x, a[5];
+
+	a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+
+	for (k = 0; k < 5; k++) {
+		gap = a[k];
+		for (i = gap; i < count; ++i) {
+			x = items[i];
+			for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+				items[j + gap] = items[j];
+			items[j + gap] = x;
+		}
+	}
+}
+
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+{
+	int i, j;
+	int x, y;
+
+
+
+	i = left; j = right;
+
+	/* выбор компаранда */
+	x = items[(left + right) / 2];
+
+	do {
+		while ((items[i] < x) && (i < right)) i++;
+		while ((x < items[j]) && (j > left)) j--;
+
+		if (i <= j) {
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++; j--;
+		}
+	} while (i <= j);
+
+	if (left < j) qs(items, left, j);
+	if (i < right) qs(items, i, right);
+}
+
 
 int main(void)
 {
@@ -19,38 +67,12 @@ int main(void)
 	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
 	while (i < size_m)
 	{
-		while (j < size_m)
-		{
-			a[i][j] = rand() % size_m + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	i = 0; j = 0;
-	while (i < size_m)
-	{
-		while (j < size_m)
-		{
-			b[i][j] = rand() % size_m + 1; // заполняем массив случайными числами
-			j++;
-		}
+		a[i] = i; // заполняем массив случайными числами
 		i++;
 	}
 
 	start = clock();
-	for (i = 0; i < size_m; i++)
-	{
-		for (j = 0; j < size_m; j++)
-		{
-			elem_c = 0;
-			for (r = 0; r < size_m; r++)
-			{
-				elem_c = elem_c + a[i][r] * b[r][j];
-				c[i][j] = elem_c;
-			}
-		}
-	}
+	qs(a, 0, size_m - 1);
 	double time = 0.0;
 	end = clock();
 	time += (double)(end - start) / CLOCKS_PER_SEC;
